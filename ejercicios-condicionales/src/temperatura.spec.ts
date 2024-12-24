5328a099d7158abb0c17e808b6a9da41072e027f
zcamcal <188593322+zcamcal@users.noreply.github.com>
2024-12-24 14:27:10 -0300

    test: fix temperatura test

--- ejercicios-condicionales/src/temperatura.spec.ts
+++ ejercicios-condicionales/src/temperatura.spec.ts
@@ -12,28 +12,24 @@ describe('clasificarTemperatura', () => {
     assert.ok(typeof resultado === 'string');
   });
 
-  it('debe retornar "Frío" para temperaturas menores a 10°C', () => {
-    assert.ok(clasificarTemperatura(0) === "Frío");
-    assert.ok(clasificarTemperatura(5) === "Frío");
+  it('debe retornar "Frío" para temperaturas menores a 0°C', () => {
+    assert.ok(clasificarTemperatura(0) !== "Frío");
+    assert.ok(clasificarTemperatura(5) !== "Frío");
     assert.ok(clasificarTemperatura(-5) === "Frío");
   });
 
-  it('debe retornar "Atemporal" para temperaturas entre 10°C y 20°C', () => {
-    assert.ok(clasificarTemperatura(10) === "Atemporal");
-    assert.ok(clasificarTemperatura(15) === "Atemporal");
-    assert.ok(clasificarTemperatura(20) === "Atemporal");
+  it('debe retornar "Templado" para temperaturas entre 0°C y 20°C', () => {
+    assert.ok(clasificarTemperatura(0) === "Templado");
+    assert.ok(clasificarTemperatura(15) === "Templado");
+    assert.ok(clasificarTemperatura(20) === "Templado");
+    assert.ok(clasificarTemperatura(30) !== "Templado");
+    assert.ok(clasificarTemperatura(-2) !== "Templado");
   });
 
-  it('debe retornar "Cálido" para temperaturas entre 20°C y 30°C', () => {
-    assert.ok(clasificarTemperatura(21) === "Cálido");
-    assert.ok(clasificarTemperatura(25) === "Cálido");
-    assert.ok(clasificarTemperatura(30) === "Cálido");
-  });
-
-  it('debe retornar "Calor" para temperaturas mayores a 30°C', () => {
-    assert.ok(clasificarTemperatura(31) === "Calor");
-    assert.ok(clasificarTemperatura(40) === "Calor");
-    assert.ok(clasificarTemperatura(45) === "Calor");
+  it('debe retornar "Caliente" para temperaturas mayores a 30°C', () => {
+    assert.ok(clasificarTemperatura(31) === "Caliente");
+    assert.ok(clasificarTemperatura(40) === "Caliente");
+    assert.ok(clasificarTemperatura(45) === "Caliente");
+    assert.ok(clasificarTemperatura(5) !== "Caliente");
   });
 });
-
